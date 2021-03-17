97a1890f1a4e6b3f44730df2113f305cf4530563
Joel Corey <joelcorey@fastmail.com>
2021-03-17 10:18:21 -0700

    favicon

--- pages/index.js
+++ pages/index.js
@@ -52,12 +52,12 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/joelcorey/portfolio"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="/img/emote-slightsmile/android-chrome-192x192.png" alt="Smiley face" className={styles.logo} />
         </a>
       </footer>
     </div>
