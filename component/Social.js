7dd66225ff44a2fe481d1040242ee001412f8339
Joel Corey <joelcorey@fastmail.com>
2021-03-20 07:03:41 -0700

    links

--- component/Social.js
+++ component/Social.js
@@ -4,21 +4,27 @@ function Social(props) {
 
 	return (
 		<div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', margin: '-13px 0 0 55px'}}>
-			<Icon 
-				path='/img/icons8-linkedin.svg'
-				width='50'
-				height='auto'
-			/>
-			<Icon 
-				path='/img/icons8-github.svg'
-				width='50'
-				height='auto'
-			/>
-			<Icon 
-				path='/img/at-sign-3010.svg'
-				width='50'
-				height='auto'
-			/>
+			<a href="https://www.linkedin.com/in/joelcorey/" target="_blank" rel="noreferrer noopener">
+				<Icon 
+					path='/img/icons8-linkedin.svg'
+					width='50'
+					height='auto'
+				/>
+			</a>
+			<a href="https://github.com/joelcorey" target="_blank" rel="noreferrer noopener">
+				<Icon 
+					path='/img/icons8-github.svg'
+					width='50'
+					height='auto'
+				/>
+			</a>
+			<a href="mailto:joelcorey@fastmail.com">
+				<Icon 
+					path='/img/at-sign-3010.svg'
+					width='50'
+					height='auto'
+				/>
+			</a>
 		</div>
 	)
 }
