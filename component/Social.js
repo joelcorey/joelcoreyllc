5da424223096b314e939515d2ef1f4c6e361648c
Joel Corey <joelcorey@fastmail.com>
2021-03-23 06:51:24 -0700

    better icon size

--- component/Social.js
+++ component/Social.js
@@ -2,26 +2,28 @@ import Icon from './Icon'
 
 function Social(props) {
 
+	let width = 40;
+
 	return (
 		<div className='flex flex-row justify-center  m-4'>
 			<a href="https://www.linkedin.com/in/joelcorey/" target="_blank" rel="noreferrer noopener">
 				<Icon 
 					path='/img/icons8-linkedin.svg'
-					width='50'
+					width={width}
 					height='auto'
 				/>
 			</a>
 			<a href="https://github.com/joelcorey" target="_blank" rel="noreferrer noopener">
 				<Icon 
 					path='/img/icons8-github.svg'
-					width='50'
+					width={width}
 					height='auto'
 				/>
 			</a>
 			<a href="mailto:joelcorey@fastmail.com">
 				<Icon 
 					path='/img/at-sign-3010.svg'
-					width='50'
+					width={width}
 					height='auto'
 				/>
 			</a>
