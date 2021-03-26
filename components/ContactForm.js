bc5f4a1298971974236fd6b3d370ae21819eadcb
Joel Corey <joelcorey@fastmail.com>
2021-03-26 09:46:29 -0700

    move functions to form component

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,49 +1,111 @@
 export default function ContactForm(props) {
 
-const { handleOnChange, handleOnSubmit } = props
+	const [status, setStatus] = useState({
+    submitted: false,
+    submitting: false,
+    info: { error: false, msg: null }
+  })
 
-return (
-	<div className="flex flex-column justify-center m-10">
-		<form className="w-full max-w-lg">
-			<div class="flex flex-wrap -mx-3 mb-6">
-				<div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-					<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-						First Name
-					</label>
-					<input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
-				</div>
-				<div class="w-full md:w-1/2 px-3">
-					<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-						Last Name
-					</label>
-					<input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
-				</div>
-				</div>
+  const [inputs, setInputs] = useState({
+    email: '',
+    message: ''
+  })
+
+  const handleResponse = (status, msg) => {
+    if (status === 200) {
+      setStatus({
+        submitted: true,
+        submitting: false,
+        info: { error: false, msg: msg }
+      })
+      setInputs({
+        email: '',
+        message: ''
+      })
+    } else {
+      setStatus({
+        info: { error: true, msg: msg }
+      })
+    }
+  }
+
+  const handleOnChange = e => {
+    e.persist()
+    setInputs(prev => ({
+      ...prev,
+      [e.target.id]: e.target.value
+    }))
+    setStatus({
+      submitted: false,
+      submitting: false,
+      info: { error: false, msg: null }
+    })
+  }
+
+  const handleOnSubmit = async e => {
+    e.preventDefault()
+    setStatus(prevStatus => ({ ...prevStatus, submitting: true }))
+    const res = await fetch('/api/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(inputs)
+    })
+    const text = await res.text()
+    handleResponse(res.status, text)
+  }
+
+	return (
+		<div className="flex flex-column justify-center m-10">
+			<form className="w-full max-w-lg">
 				<div class="flex flex-wrap -mx-3 mb-6">
-					<div class="w-full px-3">
-						<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-							E-mail
+					<div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+						<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+							First Name
 						</label>
-						<input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" />
+						<input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
 					</div>
-				</div>
-				<div class="flex flex-wrap -mx-3 mb-6">
-					<div class="w-full px-3">
-						<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-							Message
+					<div class="w-full md:w-1/2 px-3">
+						<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+							Last Name
 						</label>
-						<textarea class="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message"></textarea>
+						<input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
 					</div>
+					</div>
+					<div class="flex flex-wrap -mx-3 mb-6">
+						<div class="w-full px-3">
+							<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+								E-mail
+							</label>
+							<input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" />
+						</div>
+					</div>
+					<div class="flex flex-wrap -mx-3 mb-6">
+						<div class="w-full px-3">
+							<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+								Message
+							</label>
+							<textarea class="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message"></textarea>
+						</div>
+					</div>
+					<div class="md:flex md:items-center">
+						<div class="md:w-1/3">
+							<button class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+								Send
+							</button>
+						</div>
+					<div class="md:w-2/3"></div>
 				</div>
-				<div class="md:flex md:items-center">
-					<div class="md:w-1/3">
-						<button class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-							Send
-						</button>
-					</div>
-				<div class="md:w-2/3"></div>
-			</div>
-		</form>
-	</div>
-)
+			</form>
+
+			{status.info.error && (
+				<div className="error">Error: {status.info.msg}</div>
+			)}
+			{!status.info.error && status.info.msg && (
+				<div className="success">{status.info.msg}</div>
+			)}
+
+		</div>
+	)
 }
--- pages/contact.js
+++ pages/contact.js
@@ -1,73 +1,12 @@
-import React, { useState } from 'react'
-
 import Title from '../components/Title';
 import ContactForm from '../components/ContactForm';
 
 export default function Contact() {
 
-	const [status, setStatus] = useState({
-    submitted: false,
-    submitting: false,
-    info: { error: false, msg: null }
-  })
-
-  const [inputs, setInputs] = useState({
-    email: '',
-    message: ''
-  })
-
-  const handleResponse = (status, msg) => {
-    if (status === 200) {
-      setStatus({
-        submitted: true,
-        submitting: false,
-        info: { error: false, msg: msg }
-      })
-      setInputs({
-        email: '',
-        message: ''
-      })
-    } else {
-      setStatus({
-        info: { error: true, msg: msg }
-      })
-    }
-  }
-
-  const handleOnChange = e => {
-    e.persist()
-    setInputs(prev => ({
-      ...prev,
-      [e.target.id]: e.target.value
-    }))
-    setStatus({
-      submitted: false,
-      submitting: false,
-      info: { error: false, msg: null }
-    })
-  }
-
-  const handleOnSubmit = async e => {
-    e.preventDefault()
-    setStatus(prevStatus => ({ ...prevStatus, submitting: true }))
-    const res = await fetch('/api/send', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(inputs)
-    })
-    const text = await res.text()
-    handleResponse(res.status, text)
-  }
-
 	return (
 		<>
 			<Title />
-			<ContactForm 
-				handleOnChange={handleOnChange}
-				handleOnSubmit={handleOnSubmit}
-			/>
+			<ContactForm />
 			<div className="flex justify-center m-10">
 				<p className="contact-form-light-text">If contact forms aren't your thing email me directly at: <a href="mailto:joelcorey@fastmail.com">hello@joelcorey.com</a></p>
 			</div>
