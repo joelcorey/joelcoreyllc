7582fcb7c932ba2d5b2f769f048b7d41444ce733
Joel Corey <joelcorey@fastmail.com>
2022-07-27 10:15:26 -0700

    wipe - reinitifying

--- pages/index.js
+++ pages/index.js
@@ -12,42 +12,13 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-      
+
 				<div className='sm:flex sm:flex-column lg:flex-row justify-center'>
-					<Title />
-					<Social />	
+					<p>We do junk, garbage, and debris removal.</p>
+					<p>Contact us at 503-884-7615</p>
+
 				</div>
-        
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
 			<p>This website is brand spankin' new, please allow it time to grow and evolve if you are just viewing it now.</p>
